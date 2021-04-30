ee3ecd9983a01ed85b74a686ee68a5e77fe485e2
sunkyungnoh <cellin1637@gmail.com>
2021-04-30 19:08:32 +0900

    ✅ Add : Mission 3 - 댓글 기능 구현

--- src/Pages/sunkyungnoh/Main/components/Comments.js
+++ src/Pages/sunkyungnoh/Main/components/Comments.js
@@ -2,21 +2,51 @@ import React from 'react';
 import './Comments.scss';
 
 class Comments extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      commentArr: [],
+      newCommemt: '',
+      empty: '',
+    };
+  }
+
+  addComment = e => {
+    e.preventDefault();
+    let newCommentArr = [...this.state.commentArr];
+    newCommentArr.push(this.state.newComment);
+    this.setState({
+      commentArr: newCommentArr,
+    });
+    this.setState({
+      empty: '',
+    });
+  };
+
   render() {
     return (
       <div className="comments">
         <ul className="commentList">
-          <li className="comment"></li>
+          <li className="comment">
+            {this.state.commentArr.map(i => (
+              <div>{i}</div>
+            ))}
+          </li>
         </ul>
-        <form action="" method="POST">
+        <form onSubmit={this.addComment}>
           <input
+            onChange={e => {
+              this.setState({ newComment: e.target.value });
+              this.setState({ empty: e.target.value });
+            }}
+            value={this.state.empty}
             className="commentInput"
             name="commentInput"
             method="post"
             type="text"
             placeholder="댓글 달기 ..."
           />
-          <button type="submit" className="commentBtn" disabled>
+          <button type="submit" className="commentBtn">
             게시
           </button>
         </form>
