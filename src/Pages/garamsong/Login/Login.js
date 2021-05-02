c53d15d624e829aaca6b7aca8e990edb8ea122cc
garamsong <galmi.song@gmai.com>
2021-05-02 20:28:01 +0900

    Add : Mission1,2 - 사용자 입력 데이터 저장 기능 구현, 로그인 버튼 활성화 기능 구현

--- src/Pages/garamsong/Login/Login.js
+++ src/Pages/garamsong/Login/Login.js
@@ -3,31 +3,62 @@ import { withRouter } from 'react-router-dom';
 import './Login.scss';
 
 class Login extends React.Component {
-  goToMain = () => {
-    this.props.history.push('./Main-garamsong');
+  // goToMain = () => {
+  //   this.props.history.push('./Main-garamsong');
+  // };
+  constructor() {
+    super();
+    this.state = {
+      Id: '',
+      Pw: '',
+    };
+  }
+
+  handleIdInput = e => {
+    const { Id } = this.state;
+    this.setState({
+      Id: e.target.value,
+    });
+    console.log(e.target.value);
   };
 
+  handlePwInput = e => {
+    const { Pw } = this.state;
+    this.setState({
+      Pw: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+  const;
   render() {
+    const { Id, Pw } = this.state;
+    const isValid = Id.includes('@') && Pw.length >= 6;
     return (
       <div className="LoginGaramsong">
         <section className="loginContainer">
           <div className="loginLogoContainer">
             <h1 className="loginLogo">Westagram</h1>
           </div>
-          <form className="loginInputContainer">
+          <form className="loginInputContainer" action="/Main-garamsong">
             <input
               aria-label="type ID here"
               className="loginValue loginInputValue loginId"
               type="text"
               placeholder="전화번호, 사용자이름 또는 이메일"
+              onChange={this.handleIdInput}
             />
             <input
               aria-label="type PW here"
               className="loginValue loginInputValue loginPassword"
               type="password"
               placeholder="비밀번호"
+              onChange={this.handlePwInput}
             />
-            <button className="loginValue loginBtn" onClick={this.goToMain}>
+            <button
+              className={`loginValue loginBtn ${isValid ? 'btnActive' : ''}`}
+              onClick={this.goToMain}
+              disabled={isValid ? false : true}
+            >
               로그인
             </button>
           </form>
