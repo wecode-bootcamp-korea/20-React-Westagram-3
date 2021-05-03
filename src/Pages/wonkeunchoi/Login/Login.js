ee740c4c02ae30f39224608ad7b859cc67eee480
choiwk <dbk03053@gmail.com>
2021-05-03 15:16:24 +0900

    Mission1 입력데이터저장완료

--- src/Pages/wonkeunchoi/Login/Login.js
+++ src/Pages/wonkeunchoi/Login/Login.js
@@ -2,15 +2,56 @@ import React from 'react';
 import './Login.scss';
 //import { Link } from 'react-router-dom';
 
-class Login extends React.Component {
-  goToMain = () => {
-    this.props.history.push('/main');
+class LoginWonkeunchoi extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      id: '', //앞으로 바뀔 state에 글자가 추가되기 때문입니다.
+      pw: '', // 초기 상태는 아무것도 쓰지 않은 공백이 맞겠죠?
+      loginStatus: false,
+    };
+    // goToMain = () => {
+    //   this.props.history.push('/main-wonkeunchoi');
+  }
+
+  // handleClick = e => {
+  //   this.setState({ [e.target.name]: e.target.value }, () => {
+  //     console.log(e.target);
+  //     console.log(e.target.name);
+  //     console.log(e.target.value);
+  //   });
+  // };
+
+  handleIdClick = e => {
+    this.setState(
+      {
+        id: e.target.value,
+      },
+      () => {
+        console.log(e.target.value);
+        console.log(this.state.id);
+      }
+    );
+  };
+
+  handlePwClick = e => {
+    this.setState(
+      {
+        pw: e.target.value,
+      },
+      () => {
+        console.log(e.target.value);
+      }
+    );
   };
 
   render() {
+    //const { id, pw, loginStatus } = this.state;
+    // const isEmail = id.includes('@');
+    // const isPassword = pw;
     return (
-      <main className="Login">
-        {/* <button onClick={this.goToMain}>메인으로 이동하기 HOC</button> */}
+      <main className="Login_wonkeunchoi">
+        <button onClick={this.goToMain}>메인으로 이동하기 HOC</button>
         <article>
           <div className="westagram">
             <h1>Westagram</h1>
@@ -20,19 +61,23 @@ class Login extends React.Component {
                 <label htmlFor="userid"></label>
                 <input
                   type="text"
+                  name="id"
                   placeholder="전화번호, 사용자 이름 또는 이메일"
                   required
                   id="user_id"
+                  onChange={this.handleIdClick}
                 />
 
                 <label htmlFor="password"></label>
                 <input
                   type="password"
+                  name="pw"
                   placeholder="비밀번호"
                   minLength="5"
                   maxLength="13"
                   required
                   id="password"
+                  onChange={this.handlePwClick}
                 />
 
                 <button id="btn_login" onClick={this.goToMain}>
@@ -47,7 +92,7 @@ class Login extends React.Component {
     );
   }
 }
-export default Login;
+export default LoginWonkeunchoi;
 // export default withRouter(Login);
 
 // withRouter은 함수, input으로 컴포넌트를 받고
--- src/Pages/wonkeunchoi/Main/Main.js
+++ src/Pages/wonkeunchoi/Main/Main.js
@@ -5,14 +5,22 @@ import './Main.scss';
 import './Components/Nav/Nav.scss';
 import './Styles/reset.scss';
 import './Styles/btn_font.scss';
+//import PropTypes from 'prop-types';
 
 //import { Link } from 'react-router-dom';
 
 class Main extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      color: 'red',
+    };
+  }
+
   render() {
     return (
-      <main className="Main">
-        <Nav />
+      <main className="Main_wonkeunchoi">
+        <Nav titleColor={this.state.color} />
         <div className="top_emtied_box" />
 
         <Section />
