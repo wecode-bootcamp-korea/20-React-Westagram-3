4d8aa1f56b37b8ba8b8fb80d2697b1bb58fad769
sunkyungnoh <cellin1637@gmail.com>
2021-05-07 17:26:47 +0900

    잘못사용한 state값 상수로 변경

--- src/Pages/sunkyungnoh/Login/components/Slide.js
+++ src/Pages/sunkyungnoh/Login/components/Slide.js
@@ -2,40 +2,11 @@ import React, { Component } from 'react';
 import './Slide.scss';
 
 class Slide extends Component {
-  constructor() {
-    super();
-    this.state = {
-      img: [
-        {
-          id: 1,
-          url:
-            'https://www.instagram.com/static/images/homepage/screenshot1-2x.jpg/9144d6673849.jpg',
-        },
-        {
-          id: 2,
-          url:
-            'https://www.instagram.com/static/images/homepage/screenshot2-2x.jpg/177140221987.jpg',
-        },
-        {
-          id: 3,
-          url:
-            'https://www.instagram.com/static/images/homepage/screenshot3-2x.jpg/ff2c097a681e.jpg',
-        },
-        {
-          id: 4,
-          url:
-            'https://www.instagram.com/static/images/homepage/screenshot4-2x.jpg/b27a108592d8.jpg',
-        },
-      ],
-    };
-  }
-
   render() {
-    const { img } = this.state;
     return (
       <div className="slide">
         <div className="slideImg">
-          <img alt="imgExample" src={img[0].url} />
+          <img alt="imgExample" src={IMG[0].url} />
         </div>
       </div>
     );
@@ -43,3 +14,26 @@ class Slide extends Component {
 }
 
 export default Slide;
+
+const IMG = [
+  {
+    id: 1,
+    url:
+      'https://www.instagram.com/static/images/homepage/screenshot1-2x.jpg/9144d6673849.jpg',
+  },
+  {
+    id: 2,
+    url:
+      'https://www.instagram.com/static/images/homepage/screenshot2-2x.jpg/177140221987.jpg',
+  },
+  {
+    id: 3,
+    url:
+      'https://www.instagram.com/static/images/homepage/screenshot3-2x.jpg/ff2c097a681e.jpg',
+  },
+  {
+    id: 4,
+    url:
+      'https://www.instagram.com/static/images/homepage/screenshot4-2x.jpg/b27a108592d8.jpg',
+  },
+];
