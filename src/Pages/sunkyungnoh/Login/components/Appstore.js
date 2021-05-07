023f9b41ce5cd029ca4b76400293e0a296f2b0fe
sunkyungnoh <cellin1637@gmail.com>
2021-05-07 13:13:01 +0900

    🔥 appStore.js : 불필요한 withRouter 삭제

--- src/Pages/sunkyungnoh/Login/components/Appstore.js
+++ src/Pages/sunkyungnoh/Login/components/Appstore.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import './Appstore.scss';
 
 class Appstore extends React.Component {
@@ -25,4 +25,4 @@ class Appstore extends React.Component {
   }
 }
 
-export default withRouter(Appstore);
+export default Appstore;
